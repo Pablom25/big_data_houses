2b3d662dc6df02b5a06f936a63b03b04dbb76fe8
Pablom25 <pablomaych@icloud.com>
2026-03-12 19:57:34 +0100

    ref: remove unused imports

--- scrapers/bot_big_data.py
+++ scrapers/bot_big_data.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
