7e914772c514ee5ca87b37fdc077264cae7df03e
Pablom25 <pablomaych@icloud.com>
2026-03-12 19:25:12 +0100

    feat: data loader

---
+++ data_loader.py
@@ -0,0 +1,12 @@
+import os
+import pandas as pd
+from supabase import create_client, Client
+
+SUPABASE_URL = "https://srokpjvugcjjywogqdle.supabase.co"
+SUPABASE_KEY = "sb_publishable_Zqqpe9OFj9RctP1bLqCFGQ_LNDoVuQ3"
+
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+df = pd.read_csv("/content/months.csv")
+rows = df.to_dict(orient="records")
+supabase.table("months").insert(rows).execute()
